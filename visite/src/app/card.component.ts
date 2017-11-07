215f3e6a6a47de4d975d80a9a8aedc89bda4d411
alsina jeremy <myname@hotmail.fr>
2017-11-07 16:58:58 +0100

    commit mardi

--- visite/src/app/card.component.ts
+++ visite/src/app/card.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { CardService } from "./card.service";
 import { Card } from "./Card";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
     selector: 'app-card',
